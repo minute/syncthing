c05c92d5b147d2378bce7fd0459bfb0532803137
Jakob Borg <jakob@kastelo.net>
2016-11-23 11:39:41 +0100

    wip

--- electron/main.js
+++ electron/main.js
@@ -6,95 +6,80 @@ const url = require('url')
 const path = require('path')
 
 let win
-let stURL, stAPIKey
-
-/*
-var menubar = require('menubar')
-
-var mb = menubar({
-  index: `file://${__dirname}/popup.html`,
-  width: 200,
-  height: 400,
-})
-
-mb.on('ready', function ready () {
-  startSyncthing(mb);
-})
-*/
+let st, stURL, stAPIKey
 
 app.on('ready', createWindow)
 
-function startSyncthing() {
-  var apiKey = randomAPIKey()
-  const st = child_process.spawn("/Users/jb/bin/syncthing", ["-no-browser"], {env: {
-    "STGUIADDRESS": "http://127.0.0.1:0",
-    "STGUIAPIKEY": apiKey,
-    "STNORESTART": "1",
-    "HOME": process.env["HOME"],
-  }})
-  var s = ''
-  st.stdout.on('data', (data) => {
-    const m = (''+data).match("API listening on ([0-9:.]+)")
-    if (m) {
-      stURL = `http://${m[1]}/`
-      stAPIKey = apiKey
-      win.webContents.loadURL(stURL, {extraHeaders: "X-API-Key: " + stAPIKey})
-    }
-  })
-}
+function createWindow() {
+  win = new BrowserWindow({ width: 1200, height: 800 })
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 800})
-
-  // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  //win.webContents.openDevTools()
-
-  if (stURL) {
-          win.webContents.loadURL(stURL, {extraHeaders: "X-API-Key: " + stAPIKey})
+  if (st) {
+    win.webContents.loadURL(stURL, { extraHeaders: `X-API-Key: ${stAPIKey}` })
   } else {
     startSyncthing()
   }
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-function randomAPIKey()
-{
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+function startSyncthing() {
+  stAPIKey = stAPIKey || randomAPIKey()
+  st = child_process.spawn(`${__dirname}/bin/syncthing`, ['-no-browser'], {
+    env: {
+      "STGUIADDRESS": stURL || "http://127.0.0.1:0",
+      "STGUIAPIKEY": stAPIKey,
+      "STNORESTART": "1",
+      "HOME": process.env["HOME"],
+    }
+  })
 
-    for( var i=0; i < 32; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+  st.on('exit', (code, signal) => {
+    console.log(code, signal)
+    if (code === 0) {
+      // Shutdown
+      app.quit()
+    } else {
+      // Restarting
+      startSyncthing()
+    }
+  })
 
-    return text;
+  st.stdout.on('data', (data) => {
+    process.stderr.write(data)
+    const m = ('' + data).match("API listening on ([0-9:.]+)")
+    if (m) {
+      stURL = `http://${m[1]}/`
+      win.webContents.loadURL(stURL, { extraHeaders: `X-API-Key: ${stAPIKey}` })
+    }
+  })
+}
+
+function randomAPIKey() {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < 32; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
 }
--- electron/renderer.js
+++
@@ -1,5 +0,0 @@
-const {ipcRenderer} = require('electron')
-
-ipcRenderer.on('startup', (event, arg) => {
-  console.log(arg)
-})
