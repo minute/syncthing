80b6106142047f8d82ce71de1a0670c2777c00ff
Jakob Borg <jakob@kastelo.net>
2017-11-24 19:16:41 +0100

    files *must* have a version

--- lib/model/model_test.go
+++ lib/model/model_test.go
@@ -152,6 +152,7 @@ func genFiles(n int) []protocol.FileInfo {
 			ModifiedS: t,
 			Sequence:  int64(i + 1),
 			Blocks:    []protocol.BlockInfo{{Offset: 0, Size: 100, Hash: []byte("some hash bytes")}},
+			Version:   protocol.Vector{Counters: []protocol.Counter{{ID: 42, Value: 1}}},
 		}
 	}
 
